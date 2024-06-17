284e74129ae6ac142c059ad5ef2f1563c33fedaf
rbpolim <rbpolim@gmail.com>
2024-06-17 16:30:40 -0300

    feat: create badge component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-center p-4 border-t shadow-md">
+    <footer className="flex items-center justify-center p-4 border-t shadow-md bg-white">
       <p className="text-sm text-neutral-600">
-        Criado com 🤎 por{" "}
+        Create with 💛 by{" "}
         <Link
           href="https://github.com/rbpolim"
           className="underline text-muted-foreground hover:text-primary"
